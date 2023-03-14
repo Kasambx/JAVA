0d62ce99a04a664e0133ed7cc0233e4cc2d2d779
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2023-03-14 15:44:24 +0000

    Add files via upload

---
+++ Typeconversion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Typeconversion{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+	//	long y = input.nextInt();
+	//	System.out.println("The number is:" + y);
+
+		float y = (int) (23);
+		System.out.println(y);
+
+	}
+
+
+}
